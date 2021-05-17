6e5ca9e0b5d3d6e9e4a3710576b2f13755973272
developerOmega <developerOmega@developerOmega.com>
2021-05-17 15:28:09 -0500

    Agregar metodos queries para agregrar y eliminar datos de tabla

--- src/db/postgresql.ts
+++ src/db/postgresql.ts
@@ -44,4 +44,151 @@ export default class PostgreSQL {
   public static instance(data:DbConection) {
     return this._instance || ( this._instance = new this(data) );
   }
+
+
+  // Metodo que retorna informacion de query
+  // Recibe parametros -> query:string (consulta), data:array (datos privados)  
+  public query(query:string = '', data:any[] = []) {
+    
+    return new Promise((resolve, reject) => {
+      
+      query = PostgreSQL.statements(query);
+      
+      this.connection.query( query, data, async (err:Error, results: QueryResult) => {
+  
+        if(err) {
+          reject(err.message);
+        }
+  
+        resolve(results.rows);
+  
+      });
+
+    });
+
+  }
+
+  // Metodo que retorna informacion de query
+  // Recibe parametros -> query:string (consulta), data:array (datos privados)  
+  public queryPost(query:string = '', data:any[] = []) {
+    
+    return new Promise((resolve, reject) => {
+      
+      let body:any[] = PostgreSQL.arrayData(data[0]);
+
+      query = PostgreSQL.paramsInStatement(query, body);
+      query = PostgreSQL.statementsPost(query, data[0]);
+      query = PostgreSQL.statements(query);
+
+      this.connection.query( query, body, async (err:Error, results: QueryResult) => {
+  
+        if(err) {
+          reject(err.message);
+        }
+  
+        resolve(results.rows);
+  
+      });
+
+    });
+
+  }
+
+  // Metodo que retorna la actualizacion de una tabla
+  /* 
+    Recibe parametros -> 
+      query:string = 'UPDATE table_name SET data? WHERE id=?', 
+      data:array[body:object, id:number]
+  */ 
+  public queryPatch(query:string = '', data:any[] = []) {
+    return new Promise ((resolve, reject) => {
+      let body:any = data[0];
+      
+      query = PostgreSQL.statementsPatch(query, body);
+      query = PostgreSQL.statements(query);
+
+      let colValues:string[] = Object.keys(body).map( (key:string) => body[key] );
+      colValues.push(data[1]);
+
+      this.connection.query( query, colValues, async (err: Error, results: QueryResult) => {
+  
+        if(err) {
+          reject(err.message);
+        }
+        
+        resolve(results.rows);
+  
+      });
+
+    })   
+  }
+    
+  
+  
+  
+  // Metodo que retorna query POST con los datos body
+  // Recibe parametros -> query:string (consulta), body:object (datos de creacion)
+  private static statementsPost(query:string, body:any) {
+    let set:string[] = [];
+
+    Object.keys(body).forEach( key => set.push(`${key}`) );
+    
+    let queryProto:string[] = query.split('data?');
+    queryProto.splice(1, 0, "(", set.join(', '), ")");
+    
+    return queryProto.join('');
+  }
+
+  // Metodo que retorna query UPDATE con los datos body
+  // Recibe parametros -> query:string (consulta), body:object (datos de actualizacion)
+  private static statementsPatch(query:string, body:any) {
+    let set:string[] = [];
+    
+    Object.keys(body).forEach( key => set.push(`${key}=?`) );
+    
+    let queryProto:string[] = query.split('data?');
+    queryProto.splice(1, 0, set.join(', '));
+    
+    return queryProto.join('');
+  }
+
+  // Metodo que retorna query con los placeholdres '?' remplzados a '$1', '$2', ...
+  // Recibe parametro -> data:string (query)
+  private static statements(data:string) {
+    let dataProto:string[] = data.split('?');
+    let dataChar:string;
+
+    for(let i = 0; i < dataProto.length - 1; i++) {
+      dataProto[i] += `$${i+1}` 
+    }
+
+    dataChar = dataProto.join('');
+
+    return dataChar;
+  }
+
+
+  // Metodo que convierte objeto a un array
+  // Recibe parametro -> data:object
+  private static arrayData( data:any ):any[]{
+    let body:any[] = [];
+
+    for(let prop in data) {
+      body.push(data[prop]);
+    }
+
+    return body;
+  }
+
+  // Metodo que convierte parametros de instancia en statements secretas
+  // Recibe parametros -> query:string, body:Array<string> 
+  private static paramsInStatement( query:string, body:string[] ):string{
+    
+    let queryProto:string[] = query.split('params?');
+    let params:string[] = [];
+    body.forEach( prop => params.push("?") );
+
+    queryProto.splice(1, 0, "(", params.join(','), ")");
+    return queryProto.join('');
+  }
 }
--- tests/db.test.ts
+++ tests/db.test.ts
@@ -1,9 +1,43 @@
+import { toBindingIdentifierName } from '@babel/types';
 import { db } from '../src/db/db';
 
 describe("Test the database", () => {
 
   it("it should succes db connection", () => {
     expect(db.connection._connecting).toBe(true);
-    db.connection.end();
   })
+  
+  it("it should get all data of any table", async () => {
+    expect(typeof await db.query(`SELECT * FROM admins`)).toMatch("object");
+  })
+  
+  const values = {
+    name: "Eduardo",
+    email: "eduardo@mail.com",
+    password: "qwerty123"
+  }
+
+  it("it should create a new data in any table", async () => {
+    const data:any = await db.queryPost(`INSERT INTO admins data? VALUES params? RETURNING *`, [values]);
+
+    expect(typeof data).toMatch("object");
+
+    await db.query(`DELETE FROM admins WHERE id = ?`, [data[0].id]);
+  });
+
+  it("it should udpate data in any table", async () => {
+    const valUpdate = {
+      name: "Eduardo Mendoza"
+    }
+
+    const data:any = await db.queryPost(`INSERT INTO admins data? VALUES params? RETURNING *`, [values]);
+    const dataUpdate:any = await db.queryPatch(`UPDATE admins SET data? WHERE id = ? RETURNING *`, [valUpdate, data[0].id]);
+  
+    expect(typeof dataUpdate).toMatch("object");
+
+    await db.query(`DELETE FROM admins WHERE id = ?`, [dataUpdate[0].id]);
+    db.connection.end();
+
+  });
+
 });
