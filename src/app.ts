8a9e7d17f581cdbfe3d4acc21c9bf2bfc82d7d0c
developerOmega <developerOmega@developerOmega.com>
2021-05-24 20:02:04 -0500

    Enmaquetar contenido de administrador

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import bodyParser = require('body-parser');
 import hbs = require('hbs');
 import path = require('path')
 import session = require('express-session');
+import methodOverride = require('method-override');
 
 import { SessionConf, port } from './config/config';
 
@@ -20,6 +21,16 @@ const pathPartial = path.resolve(__dirname, '../views/partials')
 hbs.registerPartials(pathPartial);
 
 server.app.set('view engine', 'hbs');
+
+// override with the X-HTTP-Method-Override header in the request
+server.app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    const method = req.body._method
+    delete req.body._method
+    return method
+  }
+}))
 
 // Session express
 server.app.use(session({
