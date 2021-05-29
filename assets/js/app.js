ce1efc1280df78d5ac22399754ee75eb2e3f7f63
developerOmega <developerOmega@developerOmega.com>
2021-05-28 23:36:35 -0500

    Agregar peticiones para subir imagenes a projects

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,7 @@ import FileOpen from "./file_script";
 const scripts = [
   new LoginScript('/admin_panel/login'),
   new FileOpen('blogs', 'main_img', '/admin_panel/blogs/show/:id'),
+  new FileOpen('projects', 'main_img', '/admin_panel/projects/show/:id'),
 ]
 
 const openScript = new OpenScript(scripts);
