bd21850336bdfa17dfd080851cb992c9a83d66a1
developerOmega <developerOmega@developerOmega.com>
2021-06-04 19:42:52 -0500

    Crear clases para animar navbar

---
+++ assets/js/app_main.js
@@ -0,0 +1,10 @@
+import OpenScript from "./open_script";
+import LoginScript from './login_script';
+
+
+const scripts = [
+  new LoginScript('/admin_panel/login')
+]
+
+const openScript = new OpenScript( scripts );
+openScript.on();
