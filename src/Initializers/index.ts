1555bee3806ae8182d8936a6189fe3c6600a39a4
developerOmega <developerOmega@developerOmega.com>
2021-06-09 12:24:37 -0500

    make it better

--- src/Initializers/index.ts
+++ src/Initializers/index.ts
@@ -4,13 +4,13 @@ import bcrypt from 'bcrypt'
 
 const params: AdminData = {
   name: "Daniel Mendoza",
-  email: "theskip98@gmail.com",
+  email: "danos98@hotmail.com",
   password: bcrypt.hashSync("qwerty123", 10),
   is_su_admin: true
 }
 
 const createAdmin = async () => {
-  const admin = await Admin.byEmail("theskip98@gmail.com");
+  const admin:any = await Admin.byEmail("theskip98@gmail.com");
   if(!admin.id) {
     await Admin.create(params)
   } 
